c4048d25bf8a9772999da9a0129deb7f7d8d473d
dedkovs <dedkovs@gmail.com>
2021-10-04 13:03:31 +0300

    solution 1

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@
 // GOOD LUCK!
 function delay(ms) {
   // your code
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-delay(3000).then(() => alert("runs after indicted time"));
+delay(3000).then(() => alert('runs after indicted time'));
 
 // 2.
 // GOOD LUCK!
@@ -19,7 +20,10 @@ function f() {
   // ...what to write here?
   // we need to call async wait() and wait to get 10
   // remember, we can't use "await"
+  return wait().then((result) => result);
 }
+
+f().then((result) => console.log(result));
 
 // 3.
 // Resolve all promises and show in console their status and values
@@ -27,11 +31,13 @@ function f() {
 // there is a similar method that will help you to resolve all promises
 // despite of their results
 // GOOD LUCK!
-const promise1 = new Promise((res) => setTimeout(() => res("done"), 1000));
-const promise2 = Promise.reject("Through error");
+const promise1 = new Promise((res) => setTimeout(() => res('done'), 1000));
+const promise2 = Promise.reject('Through error');
 const promise3 = 6;
 
 const promises = [promise1, promise2, promise3];
+
+Promise.allSettled(promises).then((result) => console.log(result));
 
 // 4.
 // You need to handle getPosts() function with Promise
@@ -39,9 +45,52 @@ const promises = [promise1, promise2, promise3];
 // receive posts and show in console, don't forget about error handling :)
 // GOOD LUCK!
 function getPosts() {
-  return fetch("https://jsonplaceholder.typicode.com/posts");
+  return fetch('https://jsonplaceholder.typicode.com/posts');
 }
 
-function handleGetPostsWithPromise() {}
+const logResult = (method, parsedResponse) =>
+  console.log(`${method}:`, parsedResponse);
 
-function handleGetPostsWithAsyncAwait() {}
+const errorString = (method, response) =>
+  `Method: ${method}\nError status: ${response.status},\nError text: ${response.statusText}`;
+
+const logFinally = (method) => console.log(`Done ${method}`);
+
+function handleGetPostsWithPromise() {
+  const method = 'handleGetPostsWithPromise';
+  const promise = () =>
+    new Promise((resolve, reject) =>
+      getPosts().then((response) => {
+        if (!response.ok) {
+          reject(errorString(method, response));
+        }
+        resolve(response.json());
+      })
+    );
+
+  promise()
+    .then((parsedResponse) => logResult(method, parsedResponse))
+    .catch((err) => console.error(err))
+    .finally(() => logFinally(method));
+}
+
+function handleGetPostsWithAsyncAwait() {
+  const method = 'handleGetPostsWithAsyncAwait';
+  const promise = async () => {
+    try {
+      const response = await getPosts();
+      if (!response.ok) throw errorString(method, response);
+      const parsedResponse = await response.json();
+      logResult(method, parsedResponse);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      logFinally(method);
+    }
+  };
+
+  promise();
+}
+
+handleGetPostsWithPromise();
+handleGetPostsWithAsyncAwait();
